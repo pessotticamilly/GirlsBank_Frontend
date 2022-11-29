f1fb01b0616f0cd1bcc3a0d416b6b872797d8ad9
pessotticamilly <camilly_pessotti@estudante.sc.senai.br>
2022-11-29 16:24:27 -0300

    commit

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -1,28 +1,57 @@
 import './Cadastro.scss'
 
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import TextField from '@mui/material/TextField';
 
 function Cadastro() {
     return (
         <div id="Cadastro">
             <div id="container01">
-                <div id="text01">
-                    <p>Seja bem-vindo(a)!</p>
+                <div id="box01">
+                    <IconButton sx={{ margin: "1rem 0 0 1rem" }} onClick={() => {
+                        window.location.href = "/"
+                    }} >
+                        <ArrowBackIcon sx={{ color: "#fff" }} />
+                    </IconButton>
                 </div>
 
-                <div id="text02">
-                    <p>Você está muito perto de fazer parte da família Girls Bank! Preencha os dados ao lado para se juntar à nós!</p>
-                </div>
+                <div id="box02">
+                    <div id="text01">
+                        <p>Seja bem-vindo(a)!</p>
+                    </div>
 
-                <div id="text03">
-                    <p>Já possui uma conta?</p>
+                    <div id="text02">
+                        <p>Você está muito perto de fazer parte da família Girls Bank! Preencha os dados ao lado para se juntar à nós.</p>
+                    </div>
 
-                    <Button variant="outlined" sx={{ borderColor: "#fff", color: "#fff" }}>Login</Button>
+                    <div id="text03">
+                        <p>Já possui uma conta?</p>
+
+                        <Button variant="outlined" sx={{ border: "1px solid #fff", color: "#fff", "&:hover": { border: "1px solid #fff", backgroundColor: "#ffffff25" } }} onClick={() => {
+                            window.location.href = "login";
+                        }}>Login</Button>
+                    </div>
                 </div>
             </div>
 
             <div id="container02">
+                <div id="text01">
+                    <p>Criar uma conta</p>
+                </div>
 
+                <div id="box01">
+                    <TextField id="nomeCompleto" type="text" label="Nome Completo" variant="outlined" sx={{ marginBottom: "2rem", width: "50%" }} />
+
+                    <TextField id="cpf" type="number" label="CPF" variant="outlined" sx={{ marginBottom: "2rem", width: "50%" }} />
+
+                    <TextField id="telefone" type="text" label="Telefone" variant="outlined" sx={{ marginBottom: "2rem", width: "50%" }} />
+
+                    <TextField id="email" type="text" label="Email" variant="outlined" sx={{ marginBottom: "2rem", width: "50%" }} />
+
+                    <TextField id="senha" type="text" label="Senha" variant="outlined" sx={{ width: "50%" }} />
+                </div>
             </div>
         </div>
     )
